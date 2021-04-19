38ec415f130adcec592b1aab7bbdbece19e21ff8
Janet <perezjanet103@gmail.com>
2021-04-18 23:27:29 -0400

    final edits

---
+++ data_pipeline/data_pipeline/.ipynb_checkpoints/views-checkpoint.py
@@ -0,0 +1,37 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "from django.http import HttpResponse\n",
+    "\n",
+    "def index(request):\n",
+    "    return HttpResponse('My homepage')"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.5"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
